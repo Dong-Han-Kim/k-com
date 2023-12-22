f19091aa4dbb7078f8b40d2e56916ce3eb4455c2
Dong-Han-Kim <dh32110@gmail.com>
2023-12-23 03:44:50 +0900

    wip

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -1,9 +1,16 @@
 import { ReactNode } from "react";
+import style from "@/app/(afterLogin)/layout.module.css";
 
 export default async function AfterLoginLayout({
   children,
 }: {
   children: ReactNode;
 }) {
-  return <div>{children}</div>;
+  return (
+    <div className={style.container}>
+      <header className={style.leftSectionWrapper}></header>
+      <div className={style.rightSectionWrapper}></div>
+      {children}
+    </div>
+  );
 }
